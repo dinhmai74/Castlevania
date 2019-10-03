c94170d61006bb5dab853deb49e5067dc14816bf
dinhmai74 <dinh.mai@nextfunc.com>
2019-10-03 09:32:23 -0700

    add lib

---
+++ cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Animation.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Animation.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/AnimationFrame.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/AnimationFrame.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/AnimationManager.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/AnimationManager.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Boundary.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Boundary.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/BoundaryFactory.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/BoundaryFactory.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/CandleFactory.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/CandleFactory.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Enemy.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Enemy.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/EnemyFactory.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/EnemyFactory.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/EnemyGhouls.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/EnemyGhouls.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Game.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Game.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/GameObject.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/GameObject.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Grid.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Grid.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/HUD.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/HUD.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Item.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Item.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/ItemFactory.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/ItemFactory.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/ItemHeart.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/ItemHeart.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SetTimeOut.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SetTimeOut.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Simon.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Simon.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Sprite.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Sprite.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SpriteManager.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SpriteManager.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Stage.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Stage.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/StageManager.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/StageManager.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Stair.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Stair.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SubWeapon.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SubWeapon.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SubWeaponAxe.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SubWeaponAxe.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SubWeaponBoomerang.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SubWeaponBoomerang.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SubWeaponFactory.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SubWeaponFactory.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SubWeaponHolyWater.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SubWeaponHolyWater.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/SweptAABB.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/SweptAABB.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/TextureManager.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/TextureManager.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/TileMap.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/TileMap.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/TilemapManager.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/TilemapManager.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Weapon.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Weapon.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/Whip.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/Whip.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/debug.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/debug.cpp.obj"
+  "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/Castlevania/main.cpp" "C:/Users/dinhmai/source/repos/dinhmai74/Castlevania/cmake-build-debug-visual-studio/CMakeFiles/Castlevania.dir/Castlevania/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Castlevania"
+  "../Castlevania/Library/Lib/x86"
+  "../Castlevania/Library/Inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
